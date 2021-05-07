4817df7108a94b59dee91b98c40d3416c96b5381
EC2 Default User <ec2-user@ip-172-31-93-204.ec2.internal>
2021-05-07 08:36:06 +0000

    finish8章

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -1,41 +1,34 @@
 class TodolistsController < ApplicationController
   def new
-    #Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する
     @list = List.new
   end
-  
-  #以下を追加
   def create
-    # 1.データを新規登録するためのインスタンス作成
     list = List.new(list_params)
-    # 2.データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 3.トップ画面へリダイレクト
     redirect_to todolist_path(list.id)
-end
+  end
+  def index
+    @lists = List.all
+  end
+  def show
+    @list = List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+  end
 
-def index
- @lists = List.all
-end
-
-def show
-  @list = List.find(params[:id])
-end
-
-def edit
-  @list = List.find(params[:id])
-end
-
-def update
-  list = List.find(params[:id])
-  list.update(list_params)
-  redirect_to todolist_path(list.id)
-end
-
-private
-#ストロングパラメータ
-def list_params
-  params.require(:list).permit(:title,:body)
-end
-
+  private
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
 end
